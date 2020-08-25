c6b73fed198adc894ce8b82af5142f224de57cb4
DerekLoveCC <zhaomingbo1010@126.com>
2020-08-25 22:48:59 +0800

    add run bat

---
+++ ILRewrite/Run.bat
@@ -0,0 +1,5 @@
+set COR_ENABLE_PROFILING=1
+set COR_PROFILER_PATH=D:\Derek\GitHub\clrprofiler\ILRewrite\x64\Debug\ILRewriteProfiler.dll
+set COR_PROFILER={FA8F1DFF-0B62-4F84-887F-ECAC69A65DD3}
+
+D:\Derek\GitHub\clrprofiler\ILRewrite\SampleApp\bin\x64\Debug\SampleApp.exe
